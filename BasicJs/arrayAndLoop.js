5dff6d37d3fa41d4d55335893d5f5788ff81fe4a
hmoradiya <hardikmoradiya612@gmail.com>
2023-06-02 18:21:51 -0400

    Basic

--- BasicJs/arrayAndLoop.js
+++ BasicJs/arrayAndLoop.js
@@ -14,3 +14,12 @@ console.log(hobby.map(h => {
 // add data in array
 hobby.push("Programming");
 console.log("Add data in array : ", hobby);
+
+// coppy array
+const copyHobby = hobby.slice();
+// here slice can add data range from origial Array, also it can cut specific index of array
+console.log("Coppied Array: ", copyHobby);
+
+// copy array data using spread syntax
+const copyHobbyRange = [...hobby];
+console.log("Coppied Array using spread syntax : ", copyHobby);
